5e16fe49b588bc43a044ee1836093d2763cd59ff
sl_maqinglong <qinglong.ma@cienet.com.cn>
2024-04-01 22:20:12 +0800

    Initial commit

---
+++ src/main/java/com/eric/service/EricServiceApplication.java
@@ -0,0 +1,13 @@
+package com.eric.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EricServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EricServiceApplication.class, args);
+    }
+
+}
